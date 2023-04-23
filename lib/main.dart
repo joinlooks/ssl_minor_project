e6e5229be6a72f45576070771e3cb15e3ae50110
Mayank Kumrawat <76204170+mayankkumrawat@users.noreply.github.com>
2023-04-23 10:53:37 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'MVVM DEMO',
+      title: 'News App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
